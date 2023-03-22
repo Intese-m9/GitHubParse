f5474ee371b7ce088aa40da1d9f6f1c7afb378d6
ivan <9527886@mail.ru>
2023-03-22 18:35:45 +0400

    Add DI (HILT) + correct use_case

--- app/src/main/java/com/example/githubparse/data/repository/RepositoryDataBase.kt
+++ app/src/main/java/com/example/githubparse/data/repository/RepositoryDataBase.kt
@@ -5,8 +5,9 @@ import androidx.room.Room
 import com.example.githubparse.data.room.GitDao
 import com.example.githubparse.data.room.GitDatabase
 import com.example.githubparse.data.room.GitUser
+import javax.inject.Inject
 
-class RepositoryDataBase {
+class RepositoryDataBase @Inject constructor() {
     private lateinit var gitDao: GitDao
     fun executeDatabase(context: Context): List<GitUser> {
         val db = Room.databaseBuilder(context, GitDatabase::class.java, "repository_database").build()//создаем базу данных
--- app/src/main/java/com/example/githubparse/data/repository/RepositoryNet.kt
+++ app/src/main/java/com/example/githubparse/data/repository/RepositoryNet.kt
@@ -3,8 +3,9 @@ package com.example.githubparse.data.repository
 import com.example.githubparse.data.api.RetrofitHelper
 import com.example.githubparse.domain.models.getlist.GitHubList
 import retrofit2.Response
+import javax.inject.Inject
 
-class RepositoryNet {
+class RepositoryNet @Inject constructor() {
     suspend fun getGitList(userName: String): Response<GitHubList>{
         return RetrofitHelper.api.getGitList(userName)
     }
