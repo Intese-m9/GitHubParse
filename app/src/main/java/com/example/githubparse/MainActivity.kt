b48804d41f6ba7aff892480bb645fa877f53e87b
i.d.balandin <9527886@mail.ru>
2026-03-24 20:58:41 +0400

    update 26.12.2025

--- app/src/main/java/com/example/githubparse/MainActivity.kt
+++ app/src/main/java/com/example/githubparse/MainActivity.kt
@@ -4,7 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun GitApp(){
+fun GitApp() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "splash") {
         composable("splash") {
@@ -39,4 +53,3 @@ fun GitApp(){
         }
     }
 }
-
--- app/src/main/java/com/example/githubparse/di/DataModule.kt
+++ app/src/main/java/com/example/githubparse/di/DataModule.kt
@@ -12,6 +12,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
